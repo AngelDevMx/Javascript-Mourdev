af5dd6f8752c5611ca437ea8ecbe5b9e5c5bb203
Luis Angel Segura <angeldevmx27@gmail.com>
2024-07-11 14:37:57 -0600

    first commmit

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,25 @@
+console.log("Hola Javascript 2")
+console.log("Hola Javascript 2")
+
+
+// console.log("Hola Javascript 2")
+
+// Esto es un comentario
+
+/*
+    Esto es un comentario
+    multilineas
+*/
+/*
+console.log("Hola Javascript 1")
+console.log("Hola Javascript 1")
+*/
+
+console.log(4)
+console.log(typeof(""))
+console.log(5+2)
+console.log(5-2)
+console.log(5*2)
+console.log(5/2)
+console.log(5%2)
+console.log(5**2)
---
+++ Basic/01-variables.js
@@ -0,0 +1,25 @@
+//var 
+// Este tipo de variable ya esta en des uso
+var helloWorld = "¡Hello world!"
+console.log(helloWorld)
+
+var helloWorld = "Hello again, Javascript"
+console.log(helloWorld)
+
+//let 
+// VAriable que si puedo cambiar su valor :)
+
+let helloWorld2 = "¡Hello world 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "Hello again, Javascript2"
+console.log(helloWorld2)
+
+//const
+
+const helloWorld3 = "¡Hello world 3!"
+console.log(helloWorld3)
+
+// este tipo de variable no se puede cambiar el valor
+//helloWorld3 = "Hello again, Javascript 3"
+//console.log(helloWorld3)
