05bde59eac555f3db9850edc09f4bb6fe6efb04d
Luis Angel Segura <angeldevmx27@gmail.com>
2024-07-26 16:42:09 -0600

    Nueva clase

--- Basic/03-begginerexercises.js
+++ Basic/03-begginerexercises.js
@@ -19,23 +19,79 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
     // #1 String
+    let typeString = "Hola"
+    
     // #2 number
+    let randomNumber = 5
+    
     // #4 booleanos
+    console.log(1>0)
     // #5 undefined
+
+    let miUndefined 
+    
     // #6 null
+    let miPaciencia = null
+    
     // #7 bigInt
+    let bigNumber = 24896214586321456n
+    
     // #8 symbol
-
+    let mySymbol = Symbol("simbolo")
+    
 // 4. Imprime por consola el valor de todas las variables
-
+    console.log(typeString)
+    console.log(randomNumber)
+    console.log(miUndefined)
+    console.log(miPaciencia)
+    console.log(bigNumber)
+    console.log(typeof(mySymbol))
+    console.log(mySymbol)
 // 5. Imprime por consola el tipo de todas las variables
-
+    console.log(typeof(typeString))
+    console.log(typeof(randomNumber))
+    console.log(typeof(miUndefined))
+    console.log(typeof(miPaciencia))
+    console.log(typeof(bigNumber))
+    console.log(typeof(mySymbol))
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+    typeString = "Nueva cadena"
+    console.log(typeString)
+    randomNumber = 10
+    console.log(randomNumber)
+    miUndefined = 3
+    console.log(miUndefined)
+    miPaciencia = 0
+    console.log(miPaciencia)
+    bigNumber = 24563214786215n
+    console.log(bigNumber)
+    mySymbol = Symbol("nuevoSimbolo")
+    console.log(mySymbol)
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
-
+    let num = Number(typeString)
+    console.log(num)
+    let str = String(randomNumber)
+    console.log(str)
+    let biggg = bigNumber.toString()
+    console.log(typeof(biggg))
+    console.log(biggg)
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const nuevoString = "Que tranza"
+const nuevoNumber = 5
+const miNuevoUndefined = undefined
+const miNuevoNull = null
+const miBigNumber = 35632456n
+const miNuevoSymbol = Symbol("ConstSymbol")
 
 // 9. A continuación, modifica los valores de las constantes
 
+nuevoString === "que onda"
+console.log(nuevoString)
+
+//Las constantes no se pueden modificar despues de su inicalización
+
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
+
+// ----> Error: Assignment to constant variable.
--- Basic/05-operatorsexercises.js
+++ Basic/05-operatorsexercises.js
@@ -5,22 +5,75 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Crea una variable para cada operación aritmética
+    let suma = 5+10
+    let resta = 5-10
+    let multiplicacion = 5*10
+    let division = 5/10
+    let modulo = 5%10
+    let potencia = 5**10
+
+    console.log(potencia)
 
 // 2. Crea una variable para cada tipo de operación de asignación,
+    let x = 2
+    x+=85
+    console.log(x)
+    x-=5
+    console.log(x)
+    x*=99
+    console.log(x)
+    x/=2
+    console.log(x)
+    x%=4
+    console.log(x)
+    x **=9
+    console.log(x)
+
+    
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
 // 3. Imprime 5 comparaciones verdades con diferentes operadores de comparación
+   console.log(true && true)
+   console.log(true || false)
+   console.log(!false)
+   console.log("Hay algo aqui" !== 0)
+   console.log(7 > 0)
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
-
+    console.log("Hola Juan" === "Hola Juanito")
+    console.log(0 != false)
+    console.log(5<0 && 10<6)
+    console.log("" == true)
+    console.log(54 === "54")
 // 5. Utiliza el operador lógico and
-
+if("Galleta" === "Galleta" && 5>2){
+    console.log("Soy una operacion")
+}
+else{
+    console.log("Masque")
+}
 // 6. Utiliza el operador lógico or
-
+let port = null
+if(port = null || 5000)
+    console.log(port)
 // 7. Combina ambos operadores lógicos
+let myBool = false
+let myBool2 = false
+//En la operacion and ambas condiciones se deben de cumplir para tener un true
+if(myBool || myBool2 && 5>0)
+{
+    console.log("Se lograron las dos condiciones")
+}else
+{
+    console.log("Solo loramoas una condicion")
+}
 
 // 8. Añade alguna negación
+    console.log(!true)
 
 // 9. Utiliza el operador ternario
-
+    let trabajo = 0
+    trabajo ? console.log("hay baro") : console.log("No hay baro") 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+
+    
